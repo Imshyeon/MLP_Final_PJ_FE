4e1269a9da031f20da7f62f7a25c543f856ac0ec
Imshyeon <kzoen0040@naver.com>
2023-11-07 20:28:40 +0900

    fog

--- finalProject/finalProject/static/js/navbar.js
+++ finalProject/finalProject/static/js/navbar.js
@@ -27,9 +27,19 @@ document.addEventListener("DOMContentLoaded",function(){
     })
 })
 
-document.addEventListener('DOMContentLoaded',function(){
-    const switchMode = document.querySelector('#switchMode')
-    switchMode.addEventListener('click',function(){
-        switchMode.classList.toggle('is-active')
-    })
-})
+// ================추가적인 고민 필요...===================
+document.addEventListener('DOMContentLoaded', function() {
+    const darkModeSwitch = document.querySelector('#darkModeSwitch');
+    const body = document.body;
+  
+    darkModeSwitch.addEventListener('click', function() {
+      body.classList.toggle('dark-mode'); // 다크 모드 클래스를 토글
+  
+      // 스위치 클릭 애니메이션을 위한 클래스 추가 및 제거
+      darkModeSwitch.classList.add('is-active');
+      setTimeout(function() {
+        darkModeSwitch.classList.remove('is-active');
+      }, 500);
+    });
+  });
+  
