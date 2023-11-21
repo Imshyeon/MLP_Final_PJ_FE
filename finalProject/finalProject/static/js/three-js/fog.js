47a0331f4a977e7925ad926ec72013f3927ffd3e
Imshyeon <kzoen0040@naver.com>
2023-11-21 17:52:53 +0900

    home

--- finalProject/finalProject/static/js/three-js/fog.js
+++ finalProject/finalProject/static/js/three-js/fog.js
@@ -2,8 +2,9 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x212529); // 컬러 바꾸기
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0,0,5)
+camera.position.set(0,0,8)
 const renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 const container = document.getElementById('threejs-container')
@@ -12,19 +13,58 @@ container.appendChild(renderer.domElement);
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(0, 0, 1);
 scene.add(directionalLight);
+// const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+// scene.add(ambientLight)
 
-const geometry01 = new THREE.BoxGeometry(1,1,1); // x, y, z
-const material01 = new THREE.MeshStandardMaterial({
+const geometry = new THREE.BoxGeometry(4,4,2); // x, y, z
+const material = new THREE.MeshStandardMaterial({
     color:0x79E5CB
 })
-const obj01 = new THREE.Mesh(geometry01,material01)
-obj01.position.x = -2;
-scene.add(obj01)
+// const obj01_t = new THREE.Mesh(geometry,material)
+// obj01_t.position.set(-9, 4 ,0)
+// scene.add(obj01_t)
+
+// const obj01_m1 = new THREE.Mesh(geometry,material)
+// obj01_m1.position.set(-9, 0 ,0)
+// scene.add(obj01_m1)
+
+const obj01_m2 = new THREE.Mesh(geometry,material)
+obj01_m2.position.set(-5, 0 ,0)
+scene.add(obj01_m2)
+
+// const obj01_b1 = new THREE.Mesh(geometry,material)
+// obj01_b1.position.set(-9, -4 ,0)
+// scene.add(obj01_b1)
+
+// const obj01_b2 = new THREE.Mesh(geometry,material)
+// obj01_b2.position.set(-5, -4 ,0)
+// scene.add(obj01_b2)
+
+// const obj02_t = new THREE.Mesh(geometry,material)
+// obj02_t.position.set(9, 4 ,0)
+// scene.add(obj02_t)
+
+// const obj02_m1 = new THREE.Mesh(geometry,material)
+// obj02_m1.position.set(9, 0 ,0)
+// scene.add(obj02_m1)
+
+const obj02_m2 = new THREE.Mesh(geometry,material)
+obj02_m2.position.set(5, 0 ,0)
+scene.add(obj02_m2)
+
+// const obj02_b1 = new THREE.Mesh(geometry,material)
+// obj02_b1.position.set(9, -4 ,0)
+// scene.add(obj02_b1)
+
+// const obj02_b2 = new THREE.Mesh(geometry,material)
+// obj02_b2.position.set(5, -4 ,0)
+// scene.add(obj02_b2)
 
 function animate() {
     requestAnimationFrame(animate);
     // Your animation logic goes here
-    obj01.rotation.y += 0.01;
+    obj01_m2.rotation.y += 0.01;
+    obj02_m2.rotation.y += 0.01;
 
     // const scrollPosition = window.scrollY;
     // if (scrollPosition > window.innerHeight) {
@@ -32,7 +72,6 @@ function animate() {
     // } else {
     //     obj01.visible = true;  // Show the object
     // }
-
     renderer.render(scene, camera);
 }
 animate();
