8290fe6011baceaec2f670720e9792af6eed720c
Imshyeon <kzoen0040@naver.com>
2023-11-22 11:15:57 +0900

    홈 디자인 -1

--- finalProject/finalProject/static/js/custom/scroll.js
+++ finalProject/finalProject/static/js/custom/scroll.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
-    window.addEventListener('scroll', function () {
-        const footer = document.querySelector('.footer-container');
-        let scrollPosition = window.innerHeight + window.scrollY;
+// document.addEventListener('DOMContentLoaded', function () {
+//     window.addEventListener('scroll', function () {
+//         const footer = document.querySelector('.footer-container');
+//         let scrollPosition = window.innerHeight + window.scrollY;
 
-        if (scrollPosition >= document.body.offsetHeight - 5) {
-            footer.classList.add('show-footer');
-        } else {
-            footer.classList.remove('show-footer');
-        }
-    });
-});
+//         if (scrollPosition >= document.body.offsetHeight - 5) {
+//             footer.classList.add('show-footer');
+//         } else {
+//             footer.classList.remove('show-footer');
+//         }
+//     });
+// });
