7751d1785bbaa949cbb9003cef44c1de63ce6cef
Imshyeon <kzoen0040@naver.com>
2023-11-28 13:34:57 +0900

    11/28

--- finalProject/finalProject/static/js/custom/home.js
+++ finalProject/finalProject/static/js/custom/home.js
@@ -1,16 +1,35 @@
-document.addEventListener('DOMContentLoaded', function () {
+// 안개
+document.addEventListener("DOMContentLoaded", function () {
+  $(window).scroll(function () {
+    const scrollTop = $(this).scrollTop();
+    const movement = scrollTop / 0.35;
+    const blurAmount = Math.min(scrollTop / 20, 10);
+    if (scrollTop < window.innerHeight) {
+      $(".fog-container-left").css("transform", `translateX(-${movement}px)`);
+      $(".fog-container-right").css("transform", `translateX(${movement}px)`);
 
-    $(window).scroll(function () {
-        const scrollTop = $(this).scrollTop()
-        const movement = scrollTop / 0.35
-        const blurAmount = Math.min(scrollTop / 20, 10)
-        if (scrollTop < window.innerHeight) {
-            $('.fog-container-left').css('transform', `translateX(-${movement}px)`)
-            $('.fog-container-right').css('transform', `translateX(${movement}px)`)
+      $(".fog-container-left, .fog-container-right").addClass("blur");
+    } else {
+      $(".background").css("display", "none");
+    }
+  });
+});
 
-            $('.fog-container-left, .fog-container-right').addClass('blur')
-        } else{
-            $('.background').css('display', 'none')
-        }
-    })
-})
+// 애니메이션
+const observer = new IntersectionObserver(callback, { threshold: 0.07 });
+const targetElements = document.querySelectorAll(".animation-target");
+targetElements.forEach((targetElement) => {
+  observer.observe(targetElement);
+});
+
+let wasInterseciong = false;
+
+function callback(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add(
+        "animate__animated","animate__slideInUp"
+      );
+    }
+  });
+}
